e7b1cd1307c66dc23053c74059b9b6934656da2f
Jessica Williams <32785700+JayellWolfe@users.noreply.github.com>
2020-03-04 12:44:06 -0500

    Add files via upload

---
+++ hn_app/hn_utils.py
@@ -0,0 +1,27 @@
+# Imports
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+
+def custom_score(sentence):
+    # Create a SentimentIntensityAnalyzer object.
+    sid_obj = SentimentIntensityAnalyzer()
+
+    # polarity_scores method of SentimentIntensityAnalyzer
+    # oject gives a sentiment dictionary.
+    # which contains pos, neg, neu, and compound scores.
+    sentiment_dict = sid_obj.polarity_scores(sentence)
+
+    return sentiment_dict['compound']
+
+
+def compound_sum_score(sentence_list):
+
+    troll_lst = []
+
+
+    for sentence in sentence_list:
+        troll_lst.append(custom_score(sentence))
+
+    troll_score = sum(troll_lst) / len(troll_lst)
+
+
+    return troll_score 
